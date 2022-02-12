7156940768e097ec5e62e8d6f311f4f57dc4a6c1
Nada-bit73 <99046134+Nada-bit73@users.noreply.github.com>
2022-02-12 15:31:28 +0300

    Add files via upload

---
+++ Challenge_1.java
@@ -0,0 +1,24 @@
+/*
+ * 
+ * use print statment to o/p N S !
+ * 
+ */
+package challenge_1;
+
+/**
+ *
+ * @Nada
+ */
+public class Challenge_1 {
+
+    public static void main(String[] args) {
+        System.out.println("NN      N      SSS      ");
+        System.out.println("N N     N     S    S  ");
+        System.out.println("N  N    N      s     ");
+        System.out.println("N   N   N       s   ");
+        System.out.println("N    N  N         s ");
+        System.out.println("N     N N     s    s ");
+        System.out.println("N      NN       sss");
+    }
+    
+}
